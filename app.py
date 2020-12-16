af4920752252a5dcf79a981f8939c316f2a31f82
truongminhphung <41823668+truongminhphung@users.noreply.github.com>
2020-12-16 08:30:34 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import requests
+import numpy as np
+
+# Thu vien cho data reprocessing
+import nltk
+import re
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import one_hot
+
+app = Flask(__name__)
+model = load_model('my_model.h5')
+lemmatizer = WordNetLemmatizer()
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('home.html', show_predictions_modal=True)
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        text = request.form['review']
+
+        corpus = []
+        #review = BeautifulSoup(text)
+        review = re.sub('[^a-zA-Z]', ' ', text)
+        review = review.lower()
+        review = review.split()
+
+        review = [lemmatizer.lemmatize(word) for word in review if not word in stopwords.words('english')]
+        review = ' '.join(review)
+        corpus.append(review)
+        print(corpus)
+
+        # Vocabulary size
+        voc_size = 10000
+        onehot_repr = [one_hot(words, voc_size) for words in corpus]
+
+        sent_length = 1450
+        embedded_docs = pad_sequences(onehot_repr, padding='pre', maxlen=sent_length)
+
+        pred = model.predict(embedded_docs)[0][0]
+        print(pred)
+        if pred >= 0.5:
+            return render_template('home.html', result='Positive')
+        else:
+            return render_template('home.html', result='Negative')
+    else:
+        return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/javascript.js
@@ -0,0 +1,99 @@
+﻿$(document).ready(function () {
+    var i = 1;
+    $("#myBtn").click(function () {
+        $("#myModal").modal();
+    });
+
+    function ktrama() {
+        var re = /^[0-9]{6}$/;
+        if ($("txtMa").val() == "") {
+            $("#tbMa").html("* Bắt buộc nhập");
+            return false;
+        }
+        if (!re.test($("#txtMa").val())) {
+            $("#tbMa").html("* Nhập mã gồm 6 chữ số");
+            return false;
+        }
+        $("#tbMa").html("*");
+        return true;
+    }
+    $("#txtMa").blur(ktrama);
+
+    function ktraten() {
+        var re = /[A-Z]/;
+        if ($("txtTen").val() == "") {
+            $("#tbTen").html("* Bắt buộc nhập");
+            return false;
+        }
+        if (!re.test($("#txtTen").val())) {
+            $("#tbTen").html("* Tên phải in hoa");
+            return false;
+        }
+        $("#tbTen").html("*");
+        return true;
+    }
+    $("#txtTen").blur(ktraten);
+
+    function ktraemail() {
+        var re = /^[A-Za-z0-9]+\@iuh\.edu\.vn$/;
+        if ($("txtEmail").val() == "") {
+            $("#tbEmail").html("* Bắt buộc nhập");
+            return false;
+        }
+        if (!re.test($("#txtEmail").val())) {
+            $("#tbEmail").html("* Viết đúng định dạng(mẫu xxx@iuh.edu.vn)");
+            return false;
+        }
+        $("#tbEmail").html("*");
+        return true;
+    }
+    $("#txtEmail").blur(ktraemail);
+
+    function ktrasdt() {
+        var re = /^[0-9]{4}\-[0-9]{3}\-[0-9]{3}/;
+        if ($("txtSDT").val() == "") {
+            $("#tbSDT").html("* Bắt buộc nhập");
+            return false;
+        }
+        if (!re.test($("#txtSDT").val())) {
+            $("#tbSDT").html("số điện thoại phải là số(theo mẫu XXXX-XXX-XXX)");
+            return false;
+        }
+        $("#tbSDT").html("*");
+        return true;
+    }
+    $("#txtSDT").blur(ktrasdt);
+
+    function ktrangay() {
+        var day = new Date($("#txtNgay").val());
+        var today = new Date();
+        if ($("#txtNgay").val() == "") {
+            $("#tbNgay").html("* Bắt buộc nhập");
+            return false;
+        }
+        today.setDate(today.getDate() + 3);
+        if (day < today) {
+            $("#tbNgay").html("* Sau ngày hiện tại 3 ngày");
+            return false;
+        }
+        $("#tbNgay").html("*");
+        return true;
+    }
+    $("#txtNgay").blur(ktrangay);
+
+    $("#SaveBtn").click(function () {
+        if (!ktrama() || !ktraten() || !ktrangay() || !ktraemail() || !ktrasdt()) {
+            $("#thongbao").html("Phải điền đúng các thông tin");
+            return false;
+        }
+        var ma = $("#txtMa").val();
+        var ten = $("#txtTen").val();
+        var ngay = $("#txtNgay").val();
+        var email = $("#txtEmail").val();
+        var sdt = $("#txtSDT").val();
+        var them = "<tr><td>" + (i++) + "</td><td>" + ma + "</td><td>" + ten + "</td><td>" + ngay + "</td><td>" + email + "</td><td>" + sdt + "</td></tr>";
+        $("table tbody").append(them);
+        $("#myModal").modal("hide");
+    });
+
+});
